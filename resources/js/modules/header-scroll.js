113871e32abe51f71bf58aaeb6bc60c2bcbc6587
Marcel Stadelmann <m@marceli.to>
2025-05-29 17:45:13 +0200

    wip

--- resources/js/modules/header-scroll.js
+++ resources/js/modules/header-scroll.js
@@ -4,36 +4,35 @@ document.addEventListener('alpine:init', () => {
   });
   
   Alpine.data('headerScroll', () => ({
-      scrollY: 0,
-      lastScrollY: 0,
-      headerVisible: true,
+    scrollY: 0,
+    lastScrollY: 0,
+    headerVisible: true,
+    init() {
+      this.scrollY = window.pageYOffset;
+      this.lastScrollY = this.scrollY;
       
-      init() {
-          this.scrollY = window.pageYOffset;
+      window.addEventListener('scroll', () => {
+        this.scrollY = window.pageYOffset;
+        
+        // Don't hide header if menu is open
+        if (this.$store.menu.isOpen) {
+          this.headerVisible = true;
           this.lastScrollY = this.scrollY;
-          
-          window.addEventListener('scroll', () => {
-              this.scrollY = window.pageYOffset;
-              
-              // Don't hide header if menu is open
-              if (this.$store.menu.isOpen) {
-                  this.headerVisible = true;
-                  this.lastScrollY = this.scrollY;
-                  return;
-              }
-              
-              // Show header if scrolling up or at the top
-              if (this.scrollY < this.lastScrollY || this.scrollY <= 100) {
-                  this.headerVisible = true;
-              } 
-              // Hide header if scrolling down and past a threshold
-              else if (this.scrollY > this.lastScrollY && this.scrollY > 100) {
-                  this.headerVisible = false;
-              }
-              
-              this.lastScrollY = this.scrollY;
-          });
-      }
+          return;
+        }
+        
+        // Show header if scrolling up or at the top
+        if (this.scrollY < this.lastScrollY || this.scrollY <= 100) {
+          this.headerVisible = true;
+        } 
+        // Hide header if scrolling down and past a threshold
+        else if (this.scrollY > this.lastScrollY && this.scrollY > 100) {
+          this.headerVisible = false;
+        }
+        
+        this.lastScrollY = this.scrollY;
+      });
+    }
   }));
   
   Alpine.data('menuToggle', () => ({
