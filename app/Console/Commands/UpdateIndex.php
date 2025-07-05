1c6437cd5470656c00bc254dadca964764ae5604
Marcel Stadelmann <m@marceli.to>
2025-07-05 09:52:12 +0200

    wip

--- app/Console/Commands/UpdateIndex.php
+++ app/Console/Commands/UpdateIndex.php
@@ -51,6 +51,27 @@ class UpdateIndex extends Command
     $content[] = $entry->get('title');
     $this->info("Title: " . $entry->get('title'));
 
+    // Handle SEO metadata
+    if ($entry->get('open_graph_title')) {
+      $content[] = $entry->get('open_graph_title');
+      $this->info("Added SEO title");
+    }
+    if ($entry->get('open_graph_description')) {
+      $content[] = $entry->get('open_graph_description');
+      $this->info("Added SEO description");
+    }
+
+    // Handle tags (taxonomy)
+    if ($entry->get('tags')) {
+      $tags = $entry->get('tags');
+      if (is_array($tags)) {
+        foreach ($tags as $tag) {
+          $content[] = is_string($tag) ? $tag : (isset($tag['title']) ? $tag['title'] : '');
+        }
+        $this->info("Added " . count($tags) . " tags");
+      }
+    }
+
     // Handle page elements for pages
     if ($entry->collection()->handle() === 'pages')
     {
@@ -64,11 +85,56 @@ class UpdateIndex extends Command
           switch ($element['type'] ?? '') {
             case 'editor':
               if (isset($element['content'])) {
-                foreach ($element['content'] as $content_block) {
-                  $this->processContentBlock($content_block, $content);
+                $this->processBardField($element['content'], $content);
+              }
+              break;
+            case 'accordion':
+              if (isset($element['accordion_elements']) && is_array($element['accordion_elements'])) {
+                foreach ($element['accordion_elements'] as $item) {
+                  if (isset($item['accordion_title'])) {
+                    $content[] = $item['accordion_title'];
+                    $this->info("Added accordion title: " . $item['accordion_title']);
+                  }
+                  if (isset($item['accordion_element_contents']) && is_array($item['accordion_element_contents'])) {
+                    foreach ($item['accordion_element_contents'] as $contentItem) {
+                      if (isset($contentItem['editor'])) {
+                        $this->processBardField($contentItem['editor'], $content);
+                      }
+                    }
+                  }
                 }
               }
-            break;
+              break;
+            case 'single_image':
+              if (isset($element['image_caption'])) {
+                $content[] = $element['image_caption'];
+                $this->info("Added image caption");
+              }
+              if (isset($element['image_alt'])) {
+                $content[] = $element['image_alt'];
+                $this->info("Added image alt text");
+              }
+              break;
+            case 'image_gallery':
+              if (isset($element['images']) && is_array($element['images'])) {
+                foreach ($element['images'] as $image) {
+                  if (isset($image['caption'])) {
+                    $content[] = $image['caption'];
+                    $this->info("Added gallery image caption");
+                  }
+                  if (isset($image['alt'])) {
+                    $content[] = $image['alt'];
+                    $this->info("Added gallery image alt text");
+                  }
+                }
+              }
+              break;
+            case 'contact_information':
+              if (isset($element['contact_text'])) {
+                $content[] = $element['contact_text'];
+                $this->info("Added contact information");
+              }
+              break;
           }
         }
       }
@@ -80,7 +146,7 @@ class UpdateIndex extends Command
     {
       if (is_array($regularContent))
       {
-        $this->processContentBlock(['content' => $regularContent], $content);
+        $this->processBardField($regularContent, $content);
       } 
       else
       {
@@ -89,10 +155,18 @@ class UpdateIndex extends Command
       $this->info("Added content field");
     }
 
-    // Handle description field
-    if ($entry->get('description'))
+    // Handle description field (especially for publications with Bard content)
+    $description = $entry->get('description');
+    if ($description)
     {
-      $content[] = $entry->get('description');
+      if (is_array($description))
+      {
+        $this->processBardField($description, $content);
+      }
+      else
+      {
+        $content[] = $description;
+      }
       $this->info("Added description field");
     }
 
@@ -141,6 +215,93 @@ class UpdateIndex extends Command
           $content[] = $item;
           $this->info("Added string content: " . substr($item, 0, 50) . "...");
         }
+      }
+    }
+  }
+
+  private function processBardField($bardContent, &$content)
+  {
+    if (!is_array($bardContent)) {
+      return;
+    }
+
+    foreach ($bardContent as $block) {
+      if (!is_array($block)) {
+        continue;
+      }
+
+      // Handle different Bard block types
+      switch ($block['type'] ?? '') {
+        case 'paragraph':
+        case 'heading':
+        case 'text':
+          if (isset($block['content'])) {
+            $this->extractTextFromBardContent($block['content'], $content);
+          }
+          break;
+        case 'bulletList':
+        case 'orderedList':
+          if (isset($block['content'])) {
+            $this->extractTextFromBardContent($block['content'], $content);
+          }
+          break;
+        case 'listItem':
+          if (isset($block['content'])) {
+            $this->extractTextFromBardContent($block['content'], $content);
+          }
+          break;
+        case 'link':
+          if (isset($block['content'])) {
+            $this->extractTextFromBardContent($block['content'], $content);
+          }
+          // Also extract link URL if needed for context
+          if (isset($block['attrs']['href'])) {
+            $content[] = $block['attrs']['href'];
+          }
+          break;
+        case 'set':
+          // Handle set elements (like custom link sets)
+          if (isset($block['attrs']['values'])) {
+            $values = $block['attrs']['values'];
+            if (isset($values['link_text'])) {
+              $content[] = $values['link_text'];
+              $this->info("Added set link text: " . $values['link_text']);
+            }
+            if (isset($values['link'])) {
+              $content[] = $values['link'];
+              $this->info("Added set link URL");
+            }
+          }
+          break;
+        default:
+          // For any other block type, try to extract text content
+          if (isset($block['text'])) {
+            $content[] = $block['text'];
+          } elseif (isset($block['content'])) {
+            $this->extractTextFromBardContent($block['content'], $content);
+          }
+      }
+    }
+  }
+
+  private function extractTextFromBardContent($bardContent, &$content)
+  {
+    if (!is_array($bardContent)) {
+      return;
+    }
+
+    foreach ($bardContent as $item) {
+      if (is_array($item)) {
+        if (isset($item['text'])) {
+          $content[] = $item['text'];
+          $this->info("Added Bard text: " . substr($item['text'], 0, 50) . "...");
+        }
+        if (isset($item['content'])) {
+          $this->extractTextFromBardContent($item['content'], $content);
+        }
+      } elseif (is_string($item)) {
+        $content[] = $item;
+        $this->info("Added Bard string: " . substr($item, 0, 50) . "...");
       }
     }
   }
