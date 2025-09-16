6585b1fee9680836ebf8fd848f5d16befb553ebf
Marcel Stadelmann <m@marceli.to>
2025-09-16 10:46:53 +0200

    wip

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,3 +13,12 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+Route::get('/run-scheduler', function () {
+  Artisan::call('schedule:run');
+  return response()->json([
+    'status' => 'success',
+    'output' => Artisan::output()
+  ]);
+});
+
